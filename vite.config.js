9d6fa1ccecaf522e875d94535d9d36dc201c15ce
vitorqueirosz <vitor.queiroz16@hotmail.com>
2022-12-27 22:07:15 -0300

    feat(setup): initial commit

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+import svgLoader from 'vite-svg-loader';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), svgLoader()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+})
